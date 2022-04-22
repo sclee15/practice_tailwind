7b4d76fd4ada245ded1d07cf540889bab39087e0
Sung-Chul Lee <scleexyz@protonmail.com>
2022-04-22 20:33:39 +0900

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      heebo: ['Heebo']
+    },
+    extend: {
+      colors: {
+        'pinky-accent': '#FF6464',
+        'dark-blue': '#21243D',
+        'light-grey': '#8695A4',
+        'sky-accent': '#00A8CC',
+        'sky-recent': '#EDF7FA'
+      }
+    },
+  },
+  plugins: [],
+}
